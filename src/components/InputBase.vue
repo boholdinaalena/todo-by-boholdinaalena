686c977e60e35b151d2a3505e5131ba445d0720a
danilafront <danila.b@qroqo.net>
2024-01-24 01:01:48 +0300

    fix

--- src/components/InputBase.vue
+++ src/components/InputBase.vue
@@ -1,29 +1,29 @@
 <template>
-    <input v-model="test" class="body-input" type="text" v-bind="$attrs" />
+    <input v-model="modelValue" class="body-input" type="text" v-bind="$attrs" />
 </template>
 
 
 <script setup>
-import { computed } from 'vue'
+// import { computed } from 'vue'
 
-const test = computed({
-    get() {
-        return props.modelValue
-    },
-    // setter
-    set(newValue) {
-        emit('update:modelValue', newValue)
-    }
-})
+// const test = computed({
+//     get() {
+//         return props.modelValue
+//     },
+//     // setter
+//     set(newValue) {
+//         emit('update:modelValue', newValue)
+//     }
+// })
 
-const props = defineProps({
-    modelValue: {
-        type: String,
-        default: ''
-    },
-})
+// const props = defineProps({
+//     modelValue: {
+//         type: String,
+//         default: ''
+//     },
+// })
 
-const emit = defineEmits(['update:modelValue'])
+// const emit = defineEmits(['update:modelValue'])
 
-
+const modelValue = defineModel()
 </script>
